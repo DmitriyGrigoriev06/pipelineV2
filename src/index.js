bc936d80438c393cbe2efed2bd64f5b274082f72
DmitriyGrigoriev06 <greed173891@gmail.com>
2025-10-17 12:53:57 +0300

    feat(app): update root response text to 'а это уже другой текст'

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (_req, res) => {
-  res.send('Это уже новая версия текста')
+  res.send('а это уже другой текст')
 })
 
 const port = process.env.PORT || 8080
