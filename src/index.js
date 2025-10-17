04ccaffbf79df578c258ba9cc93dbb050293db96
DmitriyGrigoriev06 <greed173891@gmail.com>
2025-10-17 11:51:37 +0300

    feat(app): update root response text to Russian: 'Это уже новая версия текста'

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!')
+  res.send('Это уже новая версия текста')
 })
 
 const port = process.env.PORT || 8080
